b769c88f2297d19f48af2853fe4fca4d0da1d41f
Tim Tang <tim@getlocalmeasure.com>
2015-05-22 15:16:19 +1000

    JAVA_HOME var

--- exports.sh
+++ exports.sh
@@ -5,3 +5,4 @@ export HADOOP_PREFIX=''
 export PATH=`pwd`/hadoop-binaries/pig-0.13.0/bin:$PATH
 export HADOOP_OPTS=-Xmx8192m
 export PIG_HEAPSIZE=8192
+export JAVA_HOME=/usr/lib/jvm/java-7-openjdk-amd64/
