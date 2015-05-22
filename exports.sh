a321f5221fbfe7a683162b855ad5dff0b7edb453
Tim Tang <tim@getlocalmeasure.com>
2015-05-22 15:14:42 +1000

    heap sizes

--- exports.sh
+++ exports.sh
@@ -3,3 +3,5 @@
 export HADOOP_HOME=`pwd`/hadoop-binaries/hadoop-2.4.1
 export HADOOP_PREFIX=''
 export PATH=`pwd`/hadoop-binaries/pig-0.13.0/bin:$PATH
+export HADOOP_OPTS=-Xmx8192m
+export PIG_HEAPSIZE=8192
